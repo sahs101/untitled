fc0b3ecc56ab3e806f1cb090c1f6e7cad2895ba5
Саша <sancessss@yandex.ru>
2023-04-13 21:50:40 +0300

    чёт забыл

--- src/workout.java
+++ src/workout.java
@@ -1,5 +1,32 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class workout {
     public static void main(String[] args) {
-        System.out.println("Аблао бла" +10 );
-    }
+
+        System.out.println();
+        String name = "Игорь";
+        uman test = new uman(name, 35);
+        System.out.println(test.getNameAge());
+        name = "Натали";
+        System.out.println(test.getNameAge());
+        uman test1 = new uman("Олег", 5);
+        System.out.println();
+        String ere = "qwe";
+        System.out.println(ere);
+ere = "ewq";
+        System.out.println(ere);
+
+
+       test1.printNameAge();
+       test1.swapAge(10);
+       test1.printNameAge();
+       test1.swapName("alexandro");
+       test1.printNameAge();
+
+
+
+       
+
+  }
 }
